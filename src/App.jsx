81f39712c0036c5fbee9396da50940aa9dba409b
trevinshu <trevinshu2008@gmail.com>
2022-08-12 01:59:51 -0600

    added cart context

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -14,11 +15,11 @@ function App() {
     setCartIsShown(false);
   };
   return (
-    <div className="App">
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <Meals />
-    </div>
+    </CartProvider>
   );
 }
 
